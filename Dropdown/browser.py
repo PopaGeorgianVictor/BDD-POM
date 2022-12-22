9f9ac8b47f28ba94878b46509fe72d8a580021bd
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 11:09:47 +0200

    bdd-pom

---
+++ Dropdown/browser.py
@@ -0,0 +1,15 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+
+class Browser():
+    driver = webdriver.Chrome(service=Service(GeckoDriverManager().install()))
+    driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/dropdown")
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+
+
+   def close(self):
+        self.driver.delete_all_cookies()
+        self.driver.quit()
