326dbe6063c639f4e34d8d1e573ef18eeb42d9c6
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 17:08:11 +0200

    bdd-pom

--- Dropdown/pages/dropdown1.py
+++ Dropdown/pages/dropdown1.py
@@ -2,10 +2,8 @@ from selenium.webdriver.common.by import By
 from BDD.pages.base_page import Base_page
 
 class Home_page(Base_page)
+
     DROPDOWN_CLASS= (By.ID, 'coding-language-select')
-    DROPDOWN_CSS = (By.ID, "dropdownMenuButton")
-    OPTION_CSS = (By.XPATH, '//*[@id="dropdowns"]/div[2]/div/ul/li[4]/a')
-    SITE_BTN = (By.CSS_SELECTOR, "span[title='PORTOFOLIO']")
 
     def navigate_to_page(self):
         self.driver.get()
