ec6231401b1f481807ec01761e91d815ea56de3e
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 14:49:57 +0200

    bdd-pom

--- Dropdown/pages/homepage.py
+++ Dropdown/pages/homepage.py
@@ -8,3 +8,12 @@ class Home_page(Base_page)
 
     def navigate_to_page(self):
         self.driver.get()
+
+    def test_select_using_class(self):
+        my_dropdown = self.driver.find_element(*self.DROPDOWN_CLASS)
+        dropdown_object = Select(my_dropdown)
+        dropdown_object.select_by_value('Python')
+        dropdown_object.select_by_value('Java')
+        dropdown_object.select_by_value('PHP')
+        dropdown_object.select_by_value('C#')
+        dropdown_object.select_by_value('SQL')
