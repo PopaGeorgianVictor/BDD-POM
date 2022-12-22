389dcfb639b9c07cd10b0abb5424ebaf37a46bf0
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-22 16:24:59 +0200

    bdd-pom

--- Dropdown/steps/homepage.steps.py
+++ Dropdown/steps/homepage.steps.py
@@ -4,3 +4,6 @@ from behave import *
 def step_impl(context):
     context.home_page_object.navigate_to_page()
 
+@when("I click on  '--Please choose an option--' from 'Coding Languages List'")
+def step_impl(context):
+    context.home_page_select_using_class()
