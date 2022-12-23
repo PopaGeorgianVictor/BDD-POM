0f532b0107687cf257607aa1069371549fad8968
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-23 18:30:27 +0200

    bdd-pom

---
+++ Alerts/environment.py
@@ -0,0 +1,10 @@
+from Alerts.pages.dropdowns import Alerts
+from browser import Browser
+
+def before_all(context):
+    context.browser = Browser()
+    context.alert_object = Alerts()
+
+
+def after_all(context):
+    context.browser.close()
