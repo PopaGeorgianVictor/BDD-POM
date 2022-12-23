7247afb31d833245d4d3040c266e44683fd9ede2
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-23 18:35:21 +0200

    bdd-pom

--- Alerts/pages/alerts.py
+++ Alerts/pages/alerts.py
@@ -1,7 +1,7 @@
 from selenium.webdriver.common.by import By
 from Dropdown.pages.base_page import BasePage
 from selenium.webdriver.support.select import Select
-from selenium.common import NoSuchElementException
 import time
 
 class Dropdown(BasePage):
+
