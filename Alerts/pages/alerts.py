002a23202f6e0f9ac55cf5b6d05aaedd42f23af0
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-23 18:36:33 +0200

    bdd-pom

--- Alerts/pages/alerts.py
+++ Alerts/pages/alerts.py
@@ -3,5 +3,14 @@ from Dropdown.pages.base_page import BasePage
 from selenium.webdriver.support.select import Select
 import time
 
-class Dropdown(BasePage):
+class Alerts(BasePage):
+
+    HTML_ALERT = (By.CSS_SELECTOR, "div#bootStrapAlertExample button")
+    HTML_ALERT_TEXT = (By.ID, "bootStrapAlert")
+    JS_ALERT = (By.CSS_SELECTOR, "div#jsAlertExample button")
+    JS_CONFIRM = (By.CSS_SELECTOR, "div#jsConfirmExample button")
+    JS_PROMPT = (By.CSS_SELECTOR, "div#jsPromptExample button")
+    INSERTED_TEXT = "test"
+
+
 
