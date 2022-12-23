1ddb8511bc76ebbb446087545ae57f53dfeeaa4d
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-23 10:00:24 +0200

    bdd-pom

--- Dropdown/pages/dropdowns.py
+++ Dropdown/pages/dropdowns.py
@@ -30,6 +30,25 @@ class Dropdown(BasePage):
         dropdown_object.select_by_value('C#')
         dropdown_object.select_by_value('SQL')
 
+    def click_second_dropdown(self):
+        self.driver.find_element(*self.DROPDOWN_CLASS).click()
+
+    def display_all_options_second_dropdown(self):
+        dropdown = self.driver.find_element(*self.DROPDOWN_CLASS)
+        dropdown_object = Select(dropdown)
+
+        all_options = dropdown_object.options
+        for option in all_options:
+            print(option.text)
+
+    def select_second_dropdown(self):
+        dropdown = self.driver.find_element(*self.DROPDOWN_CLASS)
+        dropdown_object = Select(dropdown)
+        dropdown_object.select_by_value('Python')
+        dropdown_object.select_by_value('Java')
+        dropdown_object.select_by_value('PHP')
+        dropdown_object.select_by_value('C#')
+        dropdown_object.select_by_value('SQL')
 
 
 
