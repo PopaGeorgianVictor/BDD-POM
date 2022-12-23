59c1e89151177ea46a907affeb932a1a347e4d2b
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-23 14:57:02 +0200

    bdd-pom

--- Dropdown/pages/dropdowns.py
+++ Dropdown/pages/dropdowns.py
@@ -1,11 +1,13 @@
 from selenium.webdriver.common.by import By
 from Dropdown.pages.base_page import BasePage
 from selenium.webdriver.support.select import Select
+import time
 
 class Dropdown(BasePage):
 
     FIRST_DROPDOWN = (By.ID, 'coding-language-select')
     SECOND_DROPDOWN = (By.ID, "dropdownMenuButton")
+    OPTION_DROPDOWN = (By.LINK_TEXT, "OVERVIEW")
 
     def navigate_to_page(self):
         self.driver.get()
@@ -31,9 +33,19 @@ class Dropdown(BasePage):
         dropdown_object.select_by_value('SQL')
 
     def click_second_dropdown(self):
-        self.driver.find_element(*self. SECOND_DROPDOWN ).click()
+        self.driver.find_element(*self.SECOND_DROPDOWN).click()
 
+    def check_link(self):
+        time.sleep(3)
+        driver.switch_to.window(driver.window_handles[1])
+        print("Second window title = " + driver.title)
 
+        try:
+            driver.find_element(By.CSS_SELECTOR, "span[title='PORTOFOLIO']")
+            print('Element exist')
+
+        except NoSuchElementException:
+            print("Element does not exist")
 
 
 
