cfe0a0b67bf05c53f91cfc7a323075ffe7597104
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-24 20:03:03 +0200

    bdd-pom

--- Radios/pages/radios.py
+++ Radios/pages/radios.py
@@ -6,6 +6,11 @@ class Radio(BasePage):
     LOCATOR_BY_VALUE = 'input[name="radio-stations"][value="{value}"]'
     RADIOS = (By.NAME, 'radio-stations')
 
+    def default_is_selected(self):
+        expected_default_value = 'rock fm'
+        default_element = self.driver.find_element(By.CSS_SELECTOR,
+                                                   self.LOCATOR_BY_VALUE.format(value=expected_default_value))
+        assert default_element.is_selected(), f"The default value of {expected_default_value} is not selected."
 
 
 
