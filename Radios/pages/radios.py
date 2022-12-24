0bf67e8c11da41348178eef17761a805905d1fe1
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-24 20:00:33 +0200

    bdd-pom

--- Radios/pages/radios.py
+++ Radios/pages/radios.py
@@ -2,6 +2,11 @@ from selenium.webdriver.common.by import By
 from Radios.pages.base_page import BasePage
 
 
-
 class Radio(BasePage):
+    LOCATOR_BY_VALUE = 'input[name="radio-stations"][value="{value}"]'
+    RADIOS = (By.NAME, 'radio-stations')
+
+
+
+
 
