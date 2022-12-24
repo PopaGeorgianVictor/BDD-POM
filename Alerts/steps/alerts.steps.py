676d5fd57fe19c8b1b21f8af48b943ef4fb40033
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-24 10:45:53 +0200

    bdd-pom

--- Alerts/steps/alerts.steps.py
+++ Alerts/steps/alerts.steps.py
@@ -7,3 +7,7 @@ def step_impl(context):
 @when("I click on  'Show an alert (html alert)' button")
 def step_impl(context):
     context.alert_object.click_html_alert()
+
+@then("Is displayed an alert message , 'This is alert using just html.'")
+def step_impl(context):
+    context.alert_object.msg_html_alert()
