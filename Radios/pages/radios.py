a5b68525cdc7cb3d1befc411ed40bf4621e31281
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-24 20:09:56 +0200

    bdd-pom

--- Radios/pages/radios.py
+++ Radios/pages/radios.py
@@ -5,6 +5,11 @@ from Radios.pages.base_page import BasePage
 class Radio(BasePage):
     LOCATOR_BY_VALUE = 'input[name="radio-stations"][value="{value}"]'
     RADIOS = (By.NAME, 'radio-stations')
+    BTN1 = (By.CSS_SELECTOR, "input[value='magic fm']")
+    BTN2 = (By.CSS_SELECTOR, "input[value='radio galaxy']")
+    BTN3 = (By.CSS_SELECTOR, "input[value='europa fm']")
+    BTN4 = (By.CSS_SELECTOR, "input[value='rock fm']")
+
 
     def default_is_selected(self):
         expected_default_value = 'rock fm'
@@ -12,6 +17,10 @@ class Radio(BasePage):
                                                    self.LOCATOR_BY_VALUE.format(value=expected_default_value))
         assert default_element.is_selected(), f"The default value of {expected_default_value} is not selected."
 
-
+    def click_all_btn(self):
+        self.driver.find_element.click()
+        self.driver.find_element.click()
+        self.driver.find_element.click()
+        self.driver.find_element.click()
 
 
