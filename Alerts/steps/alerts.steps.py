bdbcbe1009da4d39ababc4897569d187b2f92145
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-24 10:42:53 +0200

    bdd-pom

--- Alerts/steps/alerts.steps.py
+++ Alerts/steps/alerts.steps.py
@@ -3,3 +3,7 @@ from behave import *
 @given("I am on '/alert_confirm_prompt' homepage")
 def step_impl(context):
     context.dropdown_object.navigate_to_page()
+
+@when("I click on  'Show an alert (html alert)' button")
+def step_impl(context):
+    context.alert_object.click_html_alert()
