0e2919643a3c38de08242e22c874ab8a42fbc2f0
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 11:17:55 +0200

    bdd-pom

--- Checkboxes/pages/checkboxes.py
+++ Checkboxes/pages/checkboxes.py
@@ -2,7 +2,16 @@ from Dropdown.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class Checkbox(BasePage):
+
     TO_SELECT_VALUE = '21-40'
     LOCATOR_BY_VALUE = f'input[name="age-group-checkbox"][value="{TO_SELECT_VALUE}"]'
     MY_CHOICE = (By.CSS_SELECTOR, LOCATOR_BY_VALUE)
     ALL_CHECKBOXES = (By.NAME, 'age-group-checkbox')
+
+    def navigate_to_page(self):
+        self.driver.get()
+
+    def select_one_checkbox(self):
+        selected_ch =  self.driver.find_element(*self.MY_CHOICE)
+        selected_ch.click()
+        assert selected_ch.is_selected(), f"After clicking value {TO_SELECT_VALUE} it is not selected."
