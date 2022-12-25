07eb3c859b9bcc2abab1c1e9fd1a3410642fd00b
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 17:08:26 +0200

    bdd-pom

--- Search/pages/search.py
+++ Search/pages/search.py
@@ -1,7 +1,26 @@
 from Dropdown.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 from selenium.common import NoSuchElementException
+
 class Search(BasePage):
 
     SEARCH_BAR = (By.ID, "myInput")
     ELEM = (By.LINK_TEXT, "LISTS")
+
+    def search_for_elem(self):
+        self.driver.find_element(*self.SEARCH_BAR).send_keys('lists')
+        elem = self.driver.find_element(*self.ELEM)
+        actual_search = elem.text
+        expected_search = "LISTS"
+        assert actual_search == expected_search, f'Error: expected: {expected_search}, actual: {actual_search}'
+
+    def click_on_elem(self):
+        self.driver.find_element(*self.ELEM).click()
+        print("Second window title = " + self.driver.title)
+
+        try:
+            self.driver.find_element(By.CSS_SELECTOR, "a[title='Python Tutorial']")
+            print('Element exist')
+
+        except NoSuchElementException:
+            print("Element does not exist")
