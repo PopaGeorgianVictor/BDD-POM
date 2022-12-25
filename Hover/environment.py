f8886a69123eeff94ce3b91e492cb90a78aa41d4
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 20:07:58 +0200

    bdd-pom

--- Hover/environment.py
+++ Hover/environment.py
@@ -6,6 +6,5 @@ def before_all(context):
     context.browser = Browser()
     context.hover_object = Hover()
 
-
 def after_all(context):
     context.browser.close()
--- Hover/pages/hover.py
+++ Hover/pages/hover.py
@@ -9,3 +9,10 @@ class Hover(unittest.TestCase):
     MENU = (By.CSS_SELECTOR,"#container a")
     PORTOFOLIO = (By.LINK_TEXT, "Portofolio")
     ELEM = (By.LINK_TEXT, "PORTOFOLIO")
+
+    def hovering(self):
+        menu = self.driver.find_element(*self.MENU)
+        action = ActionChains(self.driver)
+        action.move_to_element(menu).perform()
+        link = self.driver.find_element(*self.PORTOFOLIO)
+        action.move_to_element(link).click().perform()
