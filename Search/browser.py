e0d81ee46bac79bfc3bbe652cd0dd7ac7f2db16a
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 16:52:52 +0200

    bdd-pom

---
+++ Search/browser.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+class Browser:
+    driver = webdriver.Chrome(service=Service(GeckoDriverManager().install()))
+    driver.get("https://popageorgianvictor.github.io/PUBLISHED-WEBPAGES/search_bar")
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+
+   def close(self):
+       self.driver.delete_all_cookies()
+       self.driver.quit()
