85ec522a9b855cba1213622b49a1fb7903536e88
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 18:12:39 +0200

    bdd-pom

--- Search/steps/search.steps.py
+++ Search/steps/search.steps.py
@@ -2,4 +2,8 @@ from behave import *
 
 @given("I am on '/search_bar' page")
 def step_impl(context):
-    context.dropdown_object.navigate_to_page()
+    context.search_object.navigate_to_page()
+
+@when("In the search bar input I type 'LISTS'")
+def step_impl(context):
+    context.search_object.search_for_elem()
