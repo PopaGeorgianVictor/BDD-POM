107dfb276a04924c681b8006c1566f4a3739cada
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 10:29:03 +0200

    bdd-pom

--- Radios/steps/radios.steps.py
+++ Radios/steps/radios.steps.py
@@ -6,4 +6,13 @@ def step_impl(context):
 
 @then("'Rock FM' button is selected by default")
 def step_impl(context):
-    context.dropdown_object.display_all_options_first_dropdown()
+    context.dropdown_object.default_is_selected()
+
+@given("I select radio buttons one by one")
+def step_impl(context):
+    context.dropdown_object.click_all_btn()
+
+@then("I can select any button")
+def step_impl(context):
+    context.dropdown_object.click_all_btn()
+
