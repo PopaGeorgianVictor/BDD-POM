4659c00523f0ed6547505df7bb8f24de0c949004
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-25 11:13:37 +0200

    bdd-pom

--- Checkboxes/pages/checkboxes.py
+++ Checkboxes/pages/checkboxes.py
@@ -2,3 +2,7 @@ from Dropdown.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class Checkbox(BasePage):
+    TO_SELECT_VALUE = '21-40'
+    LOCATOR_BY_VALUE = f'input[name="age-group-checkbox"][value="{TO_SELECT_VALUE}"]'
+    MY_CHOICE = (By.CSS_SELECTOR, LOCATOR_BY_VALUE)
+    ALL_CHECKBOXES = (By.NAME, 'age-group-checkbox')
