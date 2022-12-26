e7b402ba90e581fa8bc50063b9af1af88a9b1238
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-26 11:18:55 +0200

    bdd-pom

--- RightClick/pages/right_click.py
+++ RightClick/pages/right_click.py
@@ -1,4 +1,6 @@
-from selenium.common import WebDriverException
+import time
+from selenium.common import NoSuchElementException
+from selenium.webdriver import ActionChains
 from selenium.webdriver.common.by import By
 from Radios.pages.base_page import BasePage
 
@@ -7,3 +9,20 @@ class RightClick(BasePage):
 
     CLICK = (By.CSS_SELECTOR, "#contextMenu a")
     ELEM = (By.LINK_TEXT, 'PORTOFOLIO')
+
+    def right_click(self):
+        ActionChains(self.driver).context_click().perform()
+        self.driver.find_element(*self.CLICK).click()
+        print('I clicked on OVERVIEW')
+
+    def click_on_option(self):
+        time.sleep(3)
+        self.driver.switch_to.window(self.driver.window_handles[1])
+        print("Second window title = " + self.driver.title)
+
+        try:
+            self.driver.find_element(*self.ELEM)
+            print('Element exist')
+
+        except NoSuchElementException:
+            print("Element does not exist")
