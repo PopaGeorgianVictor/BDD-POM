046d5d0d2408116ca1f9ae4f8c241e7144460f5d
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-26 11:34:15 +0200

    bdd-pom

--- RightClick/environment.py
+++ RightClick/environment.py
@@ -3,7 +3,7 @@ from browser import Browser
 
 def before_all(context):
     context.browser = Browser()
-    context.radios_object = RightClick()
+    context.right_click_object = RightClick()
 
 def after_all(context):
     context.browser.close()
--- RightClick/steps/right_click_steps.py
+++ RightClick/steps/right_click_steps.py
@@ -2,4 +2,13 @@ from behave import *
 
 @given("I am on '/right_click_menu' page")
 def step_impl(context):
-    context.radios_object.navigate_to_page()
+    context.right_click_object.navigate_to_page()
+
+@when("I perform a right click on page")
+def step_impl(context):
+    context.right_click_object.right_click()
+
+@then("A right click menu opened")
+def step_impl(context):
+        context.right_click_object.menu_option()
+
