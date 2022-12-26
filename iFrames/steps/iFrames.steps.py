b8c10cdcfdbe19a3a951bc06aa76ee99249a8a9d
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-26 17:17:04 +0200

    bdd-pom

--- iFrames/steps/iFrames.steps.py
+++ iFrames/steps/iFrames.steps.py
@@ -2,4 +2,8 @@ from behave import *
 
 @given("I am on  '/iFrame' page")
 def step_impl(context):
-    context.dropdown_object.navigate_to_page()
+    context.iframes_object.navigate_to_page()
+
+@when("I click on outside frames button")
+def step_impl(context):
+    context.iframes_object.click_outside_iFrame()
