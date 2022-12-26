fd5f9a76aa559820b19fa9b709f0836c127bd105
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-26 16:50:43 +0200

    bdd-pom

--- iFrames/environment.py
+++ iFrames/environment.py
@@ -1,9 +1,9 @@
-from iFrames.pages.iFrames import iFrames
+from iFrames.pages.iFrames import Frames
 from browser import Browser
 
 def before_all(context):
     context.browser = Browser()
-    context.iframes_object = iFrames()
+    context.iframes_object = Frames()
 
 def after_all(context):
     context.browser.close()
--- iFrames/pages/iFrames.py
+++ iFrames/pages/iFrames.py
@@ -4,7 +4,7 @@ from selenium.webdriver.common.by import By
 from Radios.pages.base_page import BasePage
 
 
-class iFrames(BasePage)
+class Frames(BasePage):
 
     WITHOUT_FRAME = (By.ID, 'btnOutFrame')
     OF_FRAME = (By.CSS_SELECTOR, "div[id='link'] li:nth-child(1) a:nth-child(1)")
@@ -13,3 +13,31 @@ class iFrames(BasePage)
     def navigate_to_page(self):
         self.driver.get()
 
+
+    def click_outside_iFrame(self):
+        self.driver.find_element(*self.WITHOUT_FRAME).click()
+
+    def alert_outside_iFrame(self):
+        alert = self.driver.switch_to.alert
+        assert alert.text == 'Just Clicked Outside iFrame', "Should've gotten outside message"
+        alert.accept()
+
+    def test_of_iFrame(self):
+        self.driver.switch_to.frame('myFrame1')
+        self.driver.find_element(*self.OF_FRAME).click()
+        print('I clicked in frame')
+        time.sleep(3)
+        print("Second window title = " + self.driver.title)
+
+        try:
+            self.driver.find_element(*self.OF_FRAME)
+            print('Element exist')
+
+        except NoSuchElementException:
+            print("Element does not exist")
+
+    def test_len_frames(self):
+        frames = self.driver.find_elements(*self.FRAMES)
+        for frame in frames:
+            print(frame.get_attribute("id"))
+        print(len(frames))
