d992e51d9b9679b061f8299e598f26ffcd597e0a
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-26 20:25:18 +0200

    bdd-pom

--- Resizable/pages/resizable.py
+++ Resizable/pages/resizable.py
@@ -12,10 +12,9 @@ class Resize(BasePage):
     def resizable(self):
         ActionChains(self.driver).drag_and_drop_by_offset(self.driver.find_element(*self.RESIZE), 500, 500).perform()
 
+
+
     def resize_back(self):
         ActionChains(self.driver).drag_and_drop_by_offset(self.driver.find_element(*self.RESIZE), -500, -500).perform()
 
     def check_resize(self):
-        expected = {'width': 1550, 'height': 830}
-        actual = self.driver.get_window_size()
-        assert expected == actual , f"Error: expected: {expected}, actual: {actual}"
