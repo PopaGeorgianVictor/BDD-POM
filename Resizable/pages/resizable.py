b62cfbda98278d7a50ff939772908529774fd070
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2022-12-26 19:40:34 +0200

    bdd-pom

---
+++ Resizable/pages/resizable.py
@@ -0,0 +1,10 @@
+from selenium.webdriver.common.by import By
+from Radios.pages.base_page import BasePage
+from selenium.webdriver import ActionChains
+
+class Resize(BasePage):
+
+    RESIZE = (By.XPATH, '//*[@id="resizable"]/div[3]')
+
+
+
