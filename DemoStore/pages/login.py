58a29cdb19b3ec8427ad737f9f56dedd18c240ff
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-08 19:20:53 +0200

    bdd-pom

--- DemoStore/pages/login.py
+++ DemoStore/pages/login.py
@@ -7,9 +7,12 @@ class Login(BasePage):
     PASSWORD = (By.ID, "reg_password")
     REGISTER_BTN = (By.NAME, "register")
     ERROR_TEXT = (By.XPATH,"//div[@id='content']//li[1]")
+    DASHBOARD = ()
+    PASSWORD_HINT = ()
 
     def navigate_to_page(self):
         self.driver.get("http://demostore.supersqa.com/my-account/")
+
 
     def insert_email(self):
         self.driver.find_element(*self.EMAIL).send_keys("test123@gmail.com")
