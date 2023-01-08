aa99ef4319e234c77edda16e02bb7595d033b2db
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-08 17:47:02 +0200

    bdd-pom

---
+++ DemoStore/environment.py
@@ -0,0 +1,11 @@
+from DemoStore.pages.login import Login
+from DemoStore.pages.register import Register
+from browser import Browser
+
+def before_all(context):
+    context.browser = Browser()
+    context.login_object = Login()
+    context.register_object = Register()
+
+def after_all(context):
+    context.browser.close()
