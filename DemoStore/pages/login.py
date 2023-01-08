e05e9490e668a69f42dcc2d3e8c598903bbf0606
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-08 18:46:41 +0200

    bdd-pom

--- DemoStore/pages/login.py
+++ DemoStore/pages/login.py
@@ -6,7 +6,7 @@ class Login(BasePage):
     EMAIL = (By.ID, "reg_email")
     PASSWORD = (By.ID, "reg_password")
     REGISTER_BTN = (By.NAME, "register")
-
+    INVALID_EMAIL_TEXT = (By.CSS_SELECTOR,"//div[@id='content']//li[1]")
 
 
 
