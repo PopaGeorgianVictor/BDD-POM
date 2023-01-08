419ff6fe7308013f91106c99b147dd39f9c35e8c
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-08 17:42:03 +0200

    bdd-pom

---
+++ DemoStore/browser.py
@@ -0,0 +1,13 @@
+from selenium import webdriver
+from webdriver_manager.firefox import GeckoDriverManager
+from selenium.webdriver.firefox.service import Service
+
+class Browser:
+    driver = webdriver.Chrome(service=Service(GeckoDriverManager().install()))
+    driver.get("http://demostore.supersqa.com")
+    driver.maximize_window()
+    driver.implicitly_wait(3)
+
+   def close(self):
+       self.driver.delete_all_cookies()
+       self.driver.quit()
