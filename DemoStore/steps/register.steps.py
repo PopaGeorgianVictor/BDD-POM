fac593c8b5f2451c97a5dc665c983633856293ff
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-09 17:40:48 +0200

    bdd-pom

--- DemoStore/steps/register.steps.py
+++ DemoStore/steps/register.steps.py
@@ -3,3 +3,8 @@ from behave import *
 @given("I am a user on demostore My account page")
 def step_impl(context):
     context.register_object.navigate_to_page()
+
+@when("I complete Email address field with a right address")
+def step_impl(context):
+    context.register_object.insert_email()
+
