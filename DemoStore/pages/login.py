2b2aa266502a645852f1fb51042aa05595dc7c38
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-09 18:57:00 +0200

    bdd-pom

--- DemoStore/pages/login.py
+++ DemoStore/pages/login.py
@@ -3,6 +3,8 @@ from DemoStore.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
 class Login(BasePage):
+    EMAIL = (By.ID, "username")
+    PASSWORD = (By.ID, "password")
 
 
 
