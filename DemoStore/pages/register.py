d62a2965ed426bd75ddd24887d9b064e170c1201
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-09 13:49:25 +0200

    bdd-pom

--- DemoStore/pages/register.py
+++ DemoStore/pages/register.py
@@ -1,7 +1,7 @@
 from DemoStore.pages.base_page import BasePage
 from selenium.webdriver.common.by import By
 
-class Login(BasePage):
+class Register(BasePage):
 
     EMAIL = (By.ID, "reg_email")
     PASSWORD = (By.ID, "reg_password")
