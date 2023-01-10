0f55ad38ac7c9f116dfeb54358a00b2bdd4b896b
PopaGeorgianVictor <115212789+PopaGeorgianVictor@users.noreply.github.com>
2023-01-10 10:19:20 +0200

    bdd-pom

--- Alerts/steps/login.steps.py
+++ Alerts/steps/login.steps.py
@@ -3,3 +3,11 @@ from behave import *
 @given("I am a user on demostore My account page")
 def step_impl(context):
     context.login_object.navigate_to_page()
+
+@when("I complete Username or email address field with a right address")
+def step_impl(context):
+    context.login_object.insert_email()
+
+@when("I complete Password field with a right password")
+def step_impl(context):
+    context.login_object.insert_password()
